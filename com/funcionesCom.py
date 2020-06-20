453629992f9db173a296f0d48404cf8645f1e14e
Bryan Calisto <bryancalisto@yahoo.es>
2020-06-20 18:55:08 -0500

    Added LAN scan, ARP posioning, local MAC, IP MAC, NIC provider check

--- com/funcionesCom.py
+++ com/funcionesCom.py
@@ -1,7 +1,7 @@
 # Aqui se presentan varias funciones de uso comun.
 
 def presentUso():
-    print('Uso: spufi.py <opcion>')
+    print(u'Uso: spufi.py <opción>')
     print('* Si desea ver el menu de ayuda, ingrese "spufi -h"')
 
 
@@ -11,3 +11,7 @@ def presentAyuda():
     print(u'\t-h: Presenta este menú de ayuda.')
     print(u'\t-mac: Muestra MAC de interfaz local por defecto.')
     print(u'\t-pf: Activa port forwarding (solo Linux).')
+    print(u'\t-macIp: Obtiene la MAC de una NIC dada su IP.')
+    print(u'\t-s: Escanea la IP y MAC de todos los hosts de la red.')
+    print(u'\t-pArp: Poisonea la tabla ARP de un equipo destino dada IP src y dst.')
+    print(u'\t-p: Muestra hostname de una IP.')
--- spufi.py
+++ spufi.py
@@ -1,10 +1,23 @@
+import admin
 from net.activForwLinux import activarForwLinux
 from com import funcionesCom
 from net import funcionesNet
 import platform
 import sys
+import ctypes
+import os
 
 if __name__ == '__main__':
+    # Verificamos si CMD es admin
+    try:
+        isAdmin = os.getuid() == 0
+    except AttributeError:
+        isAdmin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+    if not isAdmin:
+        print('Ejecute este programa con derechos de administrador...')
+        exit(-1)
+
     # Verificamos args.
     if len(sys.argv) < 2 or len(sys.argv) > 5:
         funcionesCom.presentUso()
@@ -23,16 +36,71 @@ if __name__ == '__main__':
         exit(0)
 
     # Activar port forwarding en el equipo (Solo Linux).
-    elif sys.argv[1] == '-pf':
+    elif sys.argv[1] == '-pF':
         os = platform.system()
-        print(f'Sistema operativo {os} detectado...')
-
-        # Activamos port forwarding.
-        print('Activando port forwarding...')
 
         if os == "Linux":
+            # Activamos port forwarding.
+            print('Activando port forwarding...')
             activarForwLinux()
         else:
-            print('Configuracion disponible solo para Linux')
-
+            print('Configuración disponible sólo para Linux...')
         exit(0)
+
+    # Verificar si la NIC de un equipo esta en modo promiscuo.
+    elif sys.argv[1] == '-chkProm':
+        if len(sys.argv) != 3:
+            print('Uso: spufi.py -chkProm <ip>')
+            exit('-1')
+
+        ip = sys.argv[2].strip()
+        funcionesNet.chkPromiscIP(ip)
+
+    # Obtiene la direccion MAC de una NIC en base a su IP.
+    elif sys.argv[1] == '-macIp':
+        if len(sys.argv) != 3:
+            print('Uso: spufi.py -macIp <ip>')
+            exit(-1)
+
+        ip = sys.argv[2]
+        mac = funcionesNet.obtMacIP(ip)
+
+        if mac:
+            prov = funcionesNet.proveeNic(mac)  # Tratamos de obtener proveedor
+            if prov:
+                print(f'La MAC de {ip} es {mac} ({prov})')
+            else:
+                print(f'La MAC de {ip} es {mac}')
+        else:
+            print('No se pudo obtener la MAC...')
+
+    # Escanea todos los hosts de la red local.
+    elif sys.argv[1] == '-s':
+        funcionesNet.escanHostsLcl()
+
+
+    # Envenena la tabla ARP (Poison ARP) de un host. Necesita IP src y dst.
+    elif sys.argv[1] == '-pArp':
+        if len(sys.argv) != 3:
+            print('Uso: spufi.py -macIp <ip src> <ip src>')
+            exit(-1)
+
+        ip1 = sys.argv[2]
+
+        funcionesNet.arpPoison(ip1)
+
+    elif sys.argv[1] == '-p':
+        if len(sys.argv) != 3:
+            print('Uso: spufi.py -p <ip>')
+            exit(-1)
+
+        result = funcionesNet.hostnameXIp(sys.argv[2])
+
+        if result:
+            print(result)
+        else:
+            print('No se pudo obtener hostname')
+
+
+    else:
+        print('Opción inválida')
