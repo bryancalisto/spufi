baec46fa163527ba1649a160ebf11392ecb69b81
Bryan Calisto <bryancalisto@yahoo.es>
2021-04-28 21:32:50 -0500

    Chore: Removed -pf option

--- com/GUI.py
+++ com/GUI.py
@@ -26,7 +26,6 @@ class GUI():
         print(GUI.yellow('Options:'))
         print(f'\t{GUI.yellow("-h:")}\tShow this help menu.')
         print(f'\t{GUI.yellow("-mac:")}\tShow MAC address of this device default NIC.')
-        print(f'\t{GUI.yellow("-pf:")}\tActivate port forwarding (Linux only).')
         print(f'\t{GUI.yellow("-macIP:")}\tShow MAC address of a host\'s NIC based on IP.')
         print(f'\t{GUI.yellow("-s:")}\tShow the IP and MAC address of all the network hosts.')
         print(f'\t{GUI.yellow("-pARP:")}\tPoison the ARP table of a host given it\'s IP and some src and dst IP.')
