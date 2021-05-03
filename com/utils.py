b564f293cf5bee373846b94d5b104506efa6a3b3
Bryan Calisto <bryancalisto@yahoo.es>
2021-05-03 14:52:23 -0500

    New: Added function to detect if running inside a virtual env

--- com/utils.py
+++ com/utils.py
@@ -1,7 +1,15 @@
 import operator
+import sys
 
 
 class Utils:
     @staticmethod
     def sortListOfObj(listOfObjToSort: list, attribute: str, ascending: bool):
         listOfObjToSort.sort(key=lambda x: x[attribute], reverse=(not ascending))
+
+
+class ProgramManager:
+    @staticmethod
+    def isVenvOn():
+        prefix = getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix
+        return prefix != sys.prefix
